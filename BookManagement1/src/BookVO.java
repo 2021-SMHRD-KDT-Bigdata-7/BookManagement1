dd329123a83328fb8fec111a0caf8d0e8ffa23d7
smhrd <smhrd@DESKTOP-UH3HVES>
2021-10-20 14:00:15 +0900

    VO 기능 추가. BookManagement 클래스 추가 (구성 메소드 추후 추가예정)

---
+++ BookManagement1/src/BookVO.java
@@ -0,0 +1,57 @@
+
+public class BookVO {
+
+	String b_id;
+	String b_title;
+	String b_author;
+	String b_publisher;
+
+	public String getB_id() {
+		return b_id;
+	}
+
+	public String getB_title() {
+		return b_title;
+	}
+
+	public String getB_author() {
+		return b_author;
+	}
+
+	public String getB_publisher() {
+		return b_publisher;
+	}
+
+	public int getB_price() {
+		return b_price;
+	}
+
+	public void setB_id(String b_id) {
+		this.b_id = b_id;
+	}
+
+	public void setB_title(String b_title) {
+		this.b_title = b_title;
+	}
+
+	public void setB_author(String b_author) {
+		this.b_author = b_author;
+	}
+
+	public void setB_publisher(String b_publisher) {
+		this.b_publisher = b_publisher;
+	}
+
+	public void setB_price(int b_price) {
+		this.b_price = b_price;
+	}
+
+	int b_price;
+
+	public BookVO(String b_id, String b_title, String b_author, String b_publisher, String b_price) {
+
+	}
+	public String toString() {
+		return "Book { [일련번호 : "+this.b_id+"] [책 제목 : "+this.b_title+"] [저자 : "+this.b_author+"] [출판사 : "+this.b_publisher+"] [가격 : "+this.b_price+"]}";
+	}
+}
